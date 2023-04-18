e2fb54b17734669f90c4a5a9837457035054f08d
Yafet Suleman <yafet.suleman.ys@gmail.com>
2023-04-18 22:31:36 +0200

    set up server

---
+++ api/apiServer.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+
+module.exports = function apiServer(port) {
+    app.use((req, _res, next) => {
+        //Print method and URL of each request
+        console.log(`${req.method} ${req.url}`);
+        next();
+    });
+
+    //Parse incoming JSON requests
+    app.use(express.json());
+
+    //Allow cross app communication
+    app.use(function (_req, res, next) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        next();
+    });
+
+    app.listen(port, function () {
+        console.log(`Server listening on port ${port}`);
+    });
+};
